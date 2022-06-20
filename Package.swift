f455b975254661a3e5d16cf97da08d9fc3488097
Andrei Panfilov <andrei.panfilov@fareharbor.com>
2022-06-20 15:42:05 +0200

    Updated module version 1.16.0

---
+++ Package.swift
@@ -0,0 +1,28 @@
+// swift-tools-version:5.3
+import PackageDescription
+
+let package = Package(
+  name: "StarIO_Extension",
+    platforms: [
+      .iOS(.v10)
+    ],
+  products: [
+    .library(
+        name: "StarIO_Extension",
+        targets: ["StarIO_Extension"]
+    )
+  ],
+  dependencies: [
+    .package(
+      name: "StarIO",
+      url: "https://github.com/fh-apanfilov/StarIO",
+      "2.9.1"
+    ),
+  ],
+  targets: [
+    .binaryTarget(
+      name: "StarIO_Extension",
+      path: "StarIO_Extension.xcframework"
+    )
+  ]
+)
